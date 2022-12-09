4387c8e51fd37d2b2fd15d8213f767a6a602fb8f
maumaumau <mauriciotxai@gmail.com>
2022-12-09 17:38:21 -0600

    Inclusión de clase MoeaRegresion

---
+++ MoeaRegresion.py
@@ -0,0 +1,18 @@
+from BaseProblem import OptimProblem, ProductionRules
+from TreeCreation import TreeIndividual
+from reglasPoly import ReglasPolinomios
+from numpy import genfromtxt
+import os
+
+
+class MoeaRegresion(OptimProblem):
+    #Constructor
+    def __init__(self, inputFile: str) -> None:
+        super().__init__("regresión simbólica", ReglasPolinomios())
+
+        if not os.path.isfile(inputFile):
+            raise Exception(f"\nEl archivo '{inputFile}' de puntos NO existe.\n")
+
+        # Leer el archivo de N puntos de la forma (X, Y)
+        # Esto resultará en un arreglo de dimensión N x 2
+        self.datos = genfromtxt(inputFile, delimiter=',')
